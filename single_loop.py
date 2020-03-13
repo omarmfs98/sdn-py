9ace6cf93868728a4d596c8d0a4ba80971d70668
Omar Flórez <omarmfs98@protonmail.com>
2020-03-13 14:01:56 -0500

    fix

--- single_loop.py
+++ single_loop.py
@@ -42,26 +42,15 @@ class SingleLoopTopo(Topo):
         opc = 0
         while opc != 3:
             opc = int(raw_input("Seleccione una opcion:\n 1. Conectar Host con Switch\n 2. Conectar Switch con Switch\n 3. Ejecutar SDN"))
-            if opc == 1:
-                for h in range(n_h):
-                    print((h + 1) + ", ")
-                pos_h = int(input("Seleccione el host: "))
-
-                for s in range(n_s):
-                    print((s + 1) + ", ")
+            if opc == 1:                
+                pos_h = int(input("Seleccione el host: "))                
                 pos_s = int(input("Seleccione el switch: "))
-
-                self.addLink(hosts[pos_h - 1], switches[pos_s - 1])
-            if opc == 2:
-                for s in range(n_s):
-                    print((s + 1) + ", ")
-                pos_s1 = int(input("Seleccione el switch 1: "))
-
-                for s in range(n_s):
-                    print((s + 1) + ", ")
+                self.addLink(hosts[pos_h], switches[pos_s])
+            if opc == 2:                
+                pos_s1 = int(input("Seleccione el switch 1: "))                
                 pos_s2 = int(input("Seleccione el switch 2: "))
 
-                self.addLink(hosts[pos_s1 - 1], switches[pos_s2 - 1])
+                self.addLink(hosts[pos_s1], switches[pos_s2])
 
 if __name__ == '__main__':
     setLogLevel('info')
